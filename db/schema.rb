1ea12137c881d577e779697a568c252fcb73fd32
Phanarat Pak <phanpak@gmail.com>
2016-08-16 12:01:36 +0200

    initialize facebook sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815160853) do
+ActiveRecord::Schema.define(version: 20160816094702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,11 @@ ActiveRecord::Schema.define(version: 20160815160853) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "city"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
