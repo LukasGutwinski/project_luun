b1925dc0b9cc7cd4309edf2c0b323f0da7151c2b
Phanarat Pak <phanpak@gmail.com>
2016-08-17 12:15:40 +0200

    add my request and approval

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
 Rails.application.routes.draw do
 	devise_for :users,
 	controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-	resources :listings
+
+  resources :listings
+
   resources :listings, :only => [:index, :show] do
-    resources :requests, :except => [:index]
+    resources :requests do
+      member do
+        put :approve
+      end
+    end
   end
+
   root to: 'pages#home'
+
   mount Attachinary::Engine => "/attachinary"
+
   namespace :user do
     resources :requests, only: [:index]
+    resources :listings, only: [:index] do
+      resources :requests, :only => [:index]
+    end
   end
+
 end
