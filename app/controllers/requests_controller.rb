44c946d74c3de0ac59c20b46e0cc6cfcc9f666a5
Phanarat Pak <phanpak@gmail.com>
2016-08-19 15:48:13 +0200

    added flash messages

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -12,27 +12,33 @@ class RequestsController < ApplicationController
     @request.status = "Waiting for approval"
     @request.listing = Listing.find(params[:listing_id])
     @request.save
+    respond_to do |format|
+      format.html { redirect_to listing_path(@restaurant) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    end
+
+    def approve
+      @listing = Listing.find(params[:listing_id])
+      @request = Request.find(params[:id])
+      @request.approve
+      @request.save
+      redirect_to listing_requests_path(@listing)
+    end
+
+    def cancel_approval
+      @listing = Listing.find(params[:listing_id])
+      @request = Request.find(params[:id])
+      @request.cancel_approval
+      @request.save
+      redirect_to listing_requests_path(@listing)
+    end
+
+
+    private
+
+    def request_params
+      params.require(:request).permit(:comment)
+    end
+
   end
-
-  def approve
-    @listing = Listing.find(params[:listing_id])
-    @request = Request.find(params[:id])
-    @request.approve
-    @request.save
-  end
-
-  def cancel_approval
-    @listing = Listing.find(params[:listing_id])
-    @request = Request.find(params[:id])
-    @request.cancel_approval
-    @request.save
-  end
-
-
-  private
-
-  def request_params
-    params.require(:request).permit(:comment)
-  end
-
-end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :listing
   belongs_to :user
+  validates_uniqueness_of :user_id, :scope => [:listing_id]
 
   def approve
     self.status = "Approved"
